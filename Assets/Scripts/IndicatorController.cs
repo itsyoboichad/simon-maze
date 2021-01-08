1804c5c568cdc3d3c02aeb37adfcbe04f7ae3e11
itsyoboichad <61477907+itsyoboichad@users.noreply.github.com>
2021-01-08 10:25:33 -0600

    Files to be uploaded
    Uploading files (and practicing branches and pulls)

---
+++ Assets/Scripts/IndicatorController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IndicatorController : MonoBehaviour
+{
+    public Material redLight;
+    public Material greenLight;
+    public Material blueLight;
+    public Material yellowLight;
+    public Canvas canvas;
+
+    UIController uiController;
+    Material fire;
+    int[] path;
+    int[] playerPath;
+    int levelCount;
+    LevelMaintenance levelMaintenance;
+    Renderer render;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //path = new int[3];
+        //path[0] = Random.Range(1, 5);
+        //path[1] = Random.Range(1, 5);
+        //path[2] = Random.Range(1, 5);
+        //levelCount = 3;
+        //playerPath = new int[3];
+        levelMaintenance = new LevelMaintenance();
+        uiController = canvas.GetComponent<UIController>();
+        levelMaintenance.initializeGame(this.gameObject, uiController);
+        //Rest of this code isn't running
+        render = GetComponent<Renderer>();
+        fire = render.material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playerPassed(int doorColor)
+    {
+        levelMaintenance.playerPassed(doorColor);
+    }
+
+    public void failed(int[] path)
+    {
+        StartCoroutine(failedAnimation(path));
+    }
+
+    public IEnumerator failedAnimation(int[] path)
+    {
+        Debug.Log("failed animation playing");
+        render.material = redLight;
+        yield return new WaitForSeconds(3);
+        render.material = fire;
+        StartCoroutine(playNewPath(path));
+        //yield return null;
+    }
+
+    public void newPath(int[] path)
+    {
+        StartCoroutine(playNewPath(path));
+    }
+
+    public IEnumerator playNewPath(int[] path)
+    {
+        Debug.Log("New path playing");
+        yield return new WaitForSeconds(1.5f);
+        for (int i = 0; i < path.Length; i++)
+        {
+            if (path[i] == LevelMaintenance.GREEN)
+            {
+                render.material = greenLight;
+                yield return new WaitForSeconds(1);
+                render.material = fire;
+                yield return new WaitForSeconds(1);
+            }
+            else if (path[i] == LevelMaintenance.RED)
+            {
+                render.material = redLight;
+                yield return new WaitForSeconds(1);
+                render.material = fire;
+                yield return new WaitForSeconds(1);
+            }
+            else if (path[i] == LevelMaintenance.BLUE)
+            {
+                render.material = blueLight;
+                yield return new WaitForSeconds(1);
+                render.material = fire;
+                yield return new WaitForSeconds(1);
+            }
+            else if (path[i] == LevelMaintenance.YELLOW)
+            {
+                render.material = yellowLight;
+                yield return new WaitForSeconds(1);
+                render.material = fire;
+                yield return new WaitForSeconds(1);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/LevelMaintenance.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMaintenance
+{
+    public const int GREEN = 0;
+    public const int RED = 1;
+    public const int BLUE = 2;
+    public const int YELLOW = 3;
+
+    int[] path;
+    int[] playerPath;
+    int levelCount;
+    IndicatorController indicator;
+    UIController uiController;
+    bool newGame = true;
+
+    public void initializeGame(GameObject indicatorLight, UIController controller)
+    {
+        indicator = indicatorLight.GetComponent<IndicatorController>();
+        uiController = controller;
+        path = new int[1];
+        path[0] = Random.Range(0, 4);
+        levelCount = 1;
+        //Debug works here
+        if (newGame)
+        {
+            indicator.newPath(path); // Issue here
+            newGame = false;
+        }
+        playerPath = new int[1];
+        Debug.Log(printArray(path));
+    }
+
+    public void playerPassed(int doorColor)
+    {
+        playerPath[path.Length - levelCount] = doorColor;
+        levelCount--;
+        if (levelCount == 0)
+        {
+            // Check if player followed correct path, return boolean for yes or no
+            bool correctPath = ArraysEqual(path, playerPath);
+            if (correctPath)
+            {
+                newLevel();
+            }
+            else
+            {
+                restartLevel();
+            }
+        }
+    }
+
+    void newLevel()
+    {
+        path = nextPath(path);
+        playerPath = new int[path.Length];
+        levelCount = path.Length;
+        indicator.newPath(path); 
+        uiController.updateLevel(path.Length);
+        Debug.Log(printArray(path));
+    }
+
+    void restartLevel()
+    {
+        //levelCount = path.Length;
+        //playerPath = new int[path.Length];
+        //Debug.Log(printArray(path));
+        initializeGame(indicator.gameObject, uiController);
+        uiController.updateLevel(1);
+        indicator.failed(path);
+    }
+
+    int[] nextPath(int[] currentPath)
+    {
+        int[] newPath = new int[currentPath.Length + 1];
+        for (int i = 0; i < currentPath.Length; i++)
+        {
+            newPath[i] = currentPath[i];
+        }
+        newPath[newPath.Length - 1] = Random.Range(0, 4);
+        return newPath;
+    }
+
+    bool ArraysEqual(int[] correctPath, int[] playerPath)
+    {
+        bool pathIsCorrect = true;
+        for (int i = 0; i < correctPath.Length; i++)
+        {
+            if (correctPath[i] != playerPath[i])
+            {
+                pathIsCorrect = false;
+                break;
+            }
+        }
+        return pathIsCorrect;
+    }
+
+    string printArray(int[] a)
+    {
+        string log = "";
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (a[i] == GREEN)
+            {
+                log += "green ";
+            }
+            else if (a[i] == RED)
+            {
+                log += "red ";
+            }
+            else if (a[i] == BLUE)
+            {
+                log += "blue ";
+            }
+            else if (a[i] == YELLOW)
+            {
+                log += "yellow ";
+            }
+        }
+        return log;
+    }
+}
